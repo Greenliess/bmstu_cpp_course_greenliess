abe04f152131d3f5bbe1f650a050726d8880487b
greenliess <v-morozov00@yandex.ru>
2024-10-28 19:02:08 +0300

    rabotaet

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,29 +1,43 @@
-#include <stdio.h>
 #include "int2str.h"
+#include <limits.h>
+#include <stdlib.h>
 
-char* int2str(int num) {
-    static char buf[12];
-    int i = 0;
-    long long number = num;
-    int nega = 0;
 
-    if (number == 0) return "0";
-    if (number < 0) {nega = 1; number = -number;}
+char* int2str(int number) {
+    if (number == 0) {
+        return "0";
+    }
 
+    if (number == INT_MIN) {
+        return "-2147483648";
+    }
+    
+    char* str = calloc(13, sizeof(char));
+    int num_len = 0;
+
+    if (number < 0) {
+        num_len++;
+        str[0] = '-';
+        number *= -1;
+    }
+
+    int number_cl = number;
+    while (number_cl > 0) {
+        number_cl /= 10;
+        num_len++;
+    }
+
+    int digit = 0;
+    int count = 0;
+    char ch = 0;
     while (number > 0) {
-        buf[i++] = (number % 10) + '0';
-        number /= 10;
+        digit = number % 10;
+        number = number / 10;
+        ch = digit + '0';
+        str[num_len - 1 - count] = ch;
+        count++;
     }
+    str[num_len] = '\0';
 
-    if (nega) buf[i++] = '-';
-
-    buf[i] = '\0';
-
-    for (int j = 0; j < i / 2; j++) {
-        char temp = buf[j];
-        buf[j] = buf[i - j - 1];
-        buf[i - j - 1] = temp;
-    }
-
-    return buf;
+    return str;
 }
